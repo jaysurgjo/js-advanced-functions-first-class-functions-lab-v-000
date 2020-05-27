f5315452e34f30a7ef9c266b92bf080dd259a81e
Jordan Surgick <jordansurgick@aol.com>
2020-05-27 00:20:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const returnFirstTwoDrivers = function (drivers){
      return drivers.slice(0, 2)
 }
 
- 
+
  const returnLastTwoDrivers = function (drivers){
     return drivers.slice(2, 4)
  }
 
- const selectingDrivers = 
+ const selectingDrivers =
   [returnFirstTwoDrivers,
   returnLastTwoDrivers]
 
